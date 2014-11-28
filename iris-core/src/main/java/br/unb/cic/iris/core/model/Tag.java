c662f811467dd1892069227931244b86f2a62137
Rodrigo B Almeida <rbonifacio@Rodrigos-iMac.local>
2014-11-28 00:08:13 -0200

    Multiple folders (aka tagging support)
    Implement fake classes for compiling with tagging commands.

---
+++ iris-core/src/main/java/br/unb/cic/iris/core/model/Tag.java
@@ -0,0 +1,15 @@
+package br.unb.cic.iris.core.model;
+
+/**
+ * A dummy class to compile the program. 
+ * @TODO: Daniel Sandoval is working on this class 
+ * in another branch. Remove this comment later. 
+ */
+public class Tag {
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
