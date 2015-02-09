24d5b6f7e8e53002175fb35d58f7161522f214e4
Fausto Carvalho Marques Silva <faustocarva@gmail.com>
2015-02-09 13:24:02 -0200

    New product definitions.

---
+++ iris-spl/generate_model14.sh
@@ -0,0 +1,1 @@
+echo -e "start\n/home/fausto/Dropbox/gits/iris/iris-aspect-oriented-programming/iris-spl/project.properties.14\ny" |  VMCode 
---
+++ iris-spl/generate_model15.sh
@@ -0,0 +1,1 @@
+echo -e "start\n/home/fausto/Dropbox/gits/iris/iris-aspect-oriented-programming/iris-spl/project.properties.15\ny" |  VMCode 
