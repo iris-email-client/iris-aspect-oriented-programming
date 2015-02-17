fb4d80c5ab1628aea62c055d6b1ba5585fa28a7e
Fausto Carvalho Marques Silva <faustocarva@gmail.com>
2015-02-17 19:47:18 -0200

    We now are independent from build machine, just run two shell scripts and we are bang,banggit status

--- iris-spl/generate_products.sh
+++ iris-spl/generate_products.sh
@@ -1,1 +1,80 @@
 #! /usr/bin/env bash
+
+base_dir="iris-spl"
+products_dir="iris_products"
+current_dir=`pwd | xargs basename`
+path=`pwd | xargs dirname`
+output_log=/dev/null
+
+function test {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "error with $1" >&2
+	exit 1
+    fi
+    return $status
+}
+
+usage() {
+  echo
+  echo "Usage: $0 [-a] [-p <list_of_products>] [-h]"
+  echo
+  echo "-a - Generate all products of Iris Product Line."
+  echo "-p - Generate a list of products, delimited by \",\"."
+  echo "-h - This help text."
+  echo
+}
+
+# Ensure we are in the correct directory.
+if [ "$current_dir" != "$base_dir" ];
+then
+    echo "You must run this script fron dir \"$base_dir\""
+    exit 1
+fi
+
+# Ensure VMCode (Hephaestus) is in path
+path_to_executable=$(which VMCode)
+if [ ! -x "$path_to_executable" ] ; then
+   echo "Hephaestus is not in PATH."
+   echo "Put VMCode (Hephaestus) in PATH and then run the this script."
+fi
+
+# Ensure enough options were given.
+if [ $# -lt 1 ];
+then
+    usage
+    exit 1
+fi
+
+case $1 in
+      -h) usage
+          exit 0
+      ;;
+      -a)
+	props_count=`ls project.properties.* 2> /dev/null  | wc -l`
+	if [ $props_count -eq 0 ];
+	then
+	    echo "No project files in dir, generate project properties files first."
+	    exit 1
+	fi
+	for input in `ls project.properties.*`
+	do
+	    echo "Generating product $input..."
+	    echo -e "start\n$path/$current_dir/$input\ny" | VMCode &> $output_log
+	    if [ $? -ne 0 ]; then
+		echo "Error with VMCode, aborting!!" 
+		exit 1
+	    fi
+	done
+      ;;
+      -p) 
+	echo "Products"
+      ;;
+      ?*) 
+	echo "ERROR: Unknown option."
+          usage
+          exit 0
+      ;;
+esac
+
--- iris-spl/generate_properties.sh
+++ iris-spl/generate_properties.sh
@@ -10,16 +10,23 @@ path=`pwd | xargs dirname`
 # Ensure we are in the correct directory.
 if [ "$current_dir" != "$base_dir" ];
 then
-    echo "You must run this script fron dir \"$base_dir\""
+    echo "You must run this script fron dir \"$base_dir\"."
     exit 1
 fi
 
+# Ensure we have enough him files to proceed.
+him_count=`ls *.him 2> /dev/null  | wc -l`
+if [ $him_count -eq 0 ];
+then
+    echo "No him files in dir, generate him files first."
+    exit 1
+fi
 
 for input in `ls *.him`
 do
     echo 
     count_formated=`printf "%02d" $count`
-    printf "Generating project.properties.%02d" $count
+    printf "Generating project.properties.%02d..." $count
     file=`printf "project.properties.%02d" $count`
     touch $file
     # Do create the file for each spl product.
@@ -29,10 +36,11 @@ do
     echo "configuration-model=$path/$base_dir/configuration-model.xml" >> $file
     echo "instance-model=$path/$base_dir/$input" >> $file
     echo "source-dir=$path" >> $file
-    if [ ! -d "$path/$base_dir/$products_dir" ]; then
-	mkdir $path/$base_dir/iris_products
+    # The target-dir must not be inside your source dir, otherwise we go in an infinite loop. We choose your $HOME dir.
+    if [ ! -d "$HOME/$products_dir" ]; then
+	mkdir $HOME/iris_products
     fi
-    echo "target-dir=$path/$base_dir/$products_dir/$count_formated" >> $file
+    echo "target-dir=$HOME/$products_dir/$count_formated" >> $file
     count=$((count+1))
 done
 echo
